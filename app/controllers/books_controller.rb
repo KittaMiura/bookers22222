fa42b42fe9ab2b86d4af41d3385e14559d5cf08d
KittaMiura <112059987+roppou@users.noreply.github.com>
2022-09-24 06:50:18 +0000

    [Finish] bok51

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,57 @@
 class BooksController < ApplicationController
-  def index
-  end
-
-  def show
-  end
 
   def new
+    @book = Book.new
+    @user = current_user
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
+  end
+
+def create
+    @user = current_user
+    @books = Book.all
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+  if @book.save
+    redirect_to book_path(@book), notice: "You have created book successfully."
+  else
+    render :index
+  end
+end
+
+  def show
+    @book = Book.find(params[:id])
+    @user = current_user
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    if @book.save
+      redirect_to books_path(@book), notice: 'You have updated book successfully..'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to '/books'
+  end
+
+private
+
+  def book_params
+    params.require(:book).permit(:title, :text)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,44 @@
 class UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+     if @user.save
+      redirect_to books_path, notice: 'Welcome! You have signed up successfully.'
+    end
+  end
+
+
   def show
+    @user = User.find(params[:id])
+    @books = @user.books.all
+    @book = Book.new
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      redirect_to books_path, notice: 'You have updated user successfully.'
+    else
+      render :edit
+    end
   end
 
   def index
+    @users = User.all
+    @books = Book.all
+    @book = Book.new
+    @user = current_user
   end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image,:body)
+  end
+
 end
